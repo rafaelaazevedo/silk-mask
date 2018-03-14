71aaf62c31890af76b76735671bbc22c5bcfa192
Rafaela Azevedo <rafaela.azevedosouza@gmail.com>
2018-03-14 17:08:15 +0000

    Added activity login and main

---
+++ app/src/main/java/com/silkmask/kotlin/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.silkmask.kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.menu_toolbar))
+    }
+}
